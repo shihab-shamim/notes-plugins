60b3732f4428c8d867fb3f76a35c7f82939295b1
shihab-shamim <shihabshamim767@gmail.com>
2025-04-12 15:23:06 +0600

    solved the preview problems

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '994e2e73fa8a7ec6e3cc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'f6bfdb342ed9665a6d40');
